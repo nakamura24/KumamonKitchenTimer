114cedfc327c12a1d4490a88a53002bcfcab6131
M.Nakamura <zmf42190@gmail.com>
2012-12-28 18:37:54 +0900

    1.0.0

---
+++ assets/www/js/kitchentimer.js
@@ -0,0 +1,103 @@
+﻿    // Cordova の読み込み完了まで待機
+    //
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    // Cordova 準備完了
+    //
+    function onDeviceReady() {
+        // 処理なし
+    }
+
+    // 警告音を3回鳴らす
+    //
+    function playBeep() {
+        navigator.notification.beep(3);
+    }
+
+    // 2秒間バイブレーションさせます
+    //
+    function vibrate() {
+        navigator.notification.vibrate(2000);
+    }
+
+var timer = null;
+
+function init()
+{
+	timer = new Timer();
+	timer.init();
+}
+
+function Timer()
+{
+	this.init = function()
+	{
+		this.data = { lastTime : 0, timerID : null, doing : false };
+		this.drawTime();
+	};
+	
+	this.drawTime = function()
+	{
+		var m = String(Math.floor(this.data.lastTime / 60));
+		var s = String(this.data.lastTime % 60);
+		
+		document.getElementById('time_m').innerHTML  = m;
+		document.getElementById('time_s').innerHTML  = s;
+		
+		if(this.data.doing && this.data.lastTime <= 0)
+		{
+			this.stop();
+			playBeep();
+		}
+	}
+	
+	this.addTime = function(val)
+	{
+		this.data.lastTime += val;
+		this.drawTime();
+	}
+	
+	this.start = function()
+	{
+		if(this.data.lastTime > 0)
+		{
+			this.data.doing = true;
+			this.data.timerID = setInterval('timer.tick();',1000);
+			document.getElementById('btn_start_or_stop').value = 'ストップ';
+		}
+	}
+	
+	this.stop = function()
+	{
+		this.data.doing = false;
+		clearInterval(this.data.timerID);
+		this.data.timerID = null;
+		document.getElementById('btn_start_or_stop').value = 'スタート';
+	}
+	
+	this.startOrStop = function()
+	{
+		if(this.data.doing)
+		{
+			this.stop();
+		}	
+		else
+		{
+			this.start();
+		}
+		this.drawTime();
+	}
+	
+	this.reset = function()
+	{
+		this.stop();
+		this.data.lastTime = 0;
+		this.drawTime();
+	}
+	
+	this.tick = function()
+	{
+		this.data.lastTime--;
+		this.drawTime();
+	}
+}
---
+++ src/jp/kumamonkitchentimer/MainActivity.java
@@ -0,0 +1,13 @@
+package jp.kumamonkitchentimer;
+
+import org.apache.cordova.*;
+import android.os.Bundle;
+
+public class MainActivity extends DroidGap {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+}
