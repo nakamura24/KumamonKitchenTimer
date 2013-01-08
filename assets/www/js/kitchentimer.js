00fdaf14efef554eb97c6a80961ca208b8de7fa0
M.Nakamura <zmf42190@gmail.com>
2013-01-08 20:56:29 +0900

    1.0.2

--- assets/www/js/kitchentimer.js
+++ assets/www/js/kitchentimer.js
@@ -1,4 +1,14 @@
-﻿    // Cordova の読み込み完了まで待機
+﻿/*
+ * Copyright (C) 2012 M.Nakamura
+ *
+ * This software is licensed under a Creative Commons
+ * Attribution-NonCommercial-ShareAlike 2.1 Japan License.
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 		http://creativecommons.org/licenses/by-nc-sa/2.1/jp/legalcode
+ */
+
+    // Cordova の読み込み完了まで待機
     //
     document.addEventListener("deviceready", onDeviceReady, false);
 
--- src/jp/kumamonkitchentimer/MainActivity.java
+++ src/jp/kumamonkitchentimer/MainActivity.java
@@ -1,13 +1,33 @@
+/*
+ * Copyright (C) 2012 M.Nakamura
+ *
+ * This software is licensed under a Creative Commons
+ * Attribution-NonCommercial-ShareAlike 2.1 Japan License.
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 		http://creativecommons.org/licenses/by-nc-sa/2.1/jp/legalcode
+ */
+
 package jp.kumamonkitchentimer;
 
 import org.apache.cordova.*;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends DroidGap {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+        // Keep screen on
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);        
 		super.loadUrl("file:///android_asset/www/index.html");
+	}
+
+	@Override
+	public void onDestroy() {
+		// Keep screen off
+		getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);        
+		super.onDestroy();
 	}
 }
